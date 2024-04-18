e1a0214b5cd6bfb554c1d85abf84b1ff110bbdcd
alex931d <102577495+alex931d@users.noreply.github.com>
2024-04-18 14:22:42 -0500

    updated

--- server.ts
+++ server.ts
@@ -27,7 +27,15 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://alex931d.github.io/",
+      "https://alex931d.github.io/invoiceApp/",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(
   helmet.contentSecurityPolicy({
