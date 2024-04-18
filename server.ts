f91a8cce52f91ff454ac6b954c7efda78e7edd33
alex931d <102577495+alex931d@users.noreply.github.com>
2024-04-18 16:00:15 -0500

    updated

--- server.ts
+++ server.ts
@@ -28,12 +28,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-app.use(
-  cors({
-    origin: "https://invoiceappfrontend.onrender.com",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(
   helmet({
     referrerPolicy: {
